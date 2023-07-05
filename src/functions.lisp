46b464104faf3f8a589c498b3c9484afc3b656dd
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-07-05 19:51:35 +0100

    Working through the lisp function page

--- src/functions.lisp
+++ src/functions.lisp
@@ -5,3 +5,14 @@
     (+ (fib (- n 1)) (fib (- n 2)))
   )
 )
+
+;; functions can return multiple values i.e...
+(defun many (n)
+  (values n (* n 2) (* n 3))
+)
+;; (multiple-value-list (many 2)) -> (2, 4, 6)
+;; (nth-value 1 (many 2)) -> (4)
+;; (multiple-value-bind (first second third)
+;;   (many 2)
+;;   (list first second third)
+;;  ) -> (2, 4, 6)
